5161bd8cc730775c303dbe136c7c649ec1e1ac21
Duttoo Ervin <duttoobadree@gmail.com>
2024-05-13 15:27:00 +0200

    Add a limit of string on tag name

--- blog-hotwire/db/migrate/20240513123912_create_tag.rb
+++ blog-hotwire/db/migrate/20240513123912_create_tag.rb
@@ -1,7 +1,7 @@
 class CreateTag < ActiveRecord::Migration[7.1]
   def change
     create_table :tags do |t|
-      t.string :name
+      t.string :name, null: false, limit: 50
 
       t.timestamps
     end
