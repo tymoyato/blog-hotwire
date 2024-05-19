1d592b7a750cc4d2749d6dac462f39844c0401e3
Duttoo Ervin <duttoobadree@gmail.com>
2024-05-20 00:58:58 +0200

    Add tagify to post edit/new

--- blog-hotwire/app/javascript/application.js
+++ blog-hotwire/app/javascript/application.js
@@ -1,5 +1,20 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails";
 import "./controllers";
-
 Turbo.session.drive = false
+import Tagify from '@yaireo/tagify';
+
+// If you want to use Tagify on a specific input element
+document.addEventListener('DOMContentLoaded', () => {
+  const input = document.querySelector('input[name="post[tags]"]');
+  if (input) {
+    new Tagify(input);
+  }
+});
+
+document.addEventListener('turbo:frame-load', () => {
+  const inputs = document.querySelectorAll('input[name="post[tags]"]');
+  inputs.forEach((input) => {
+    new Tagify(input);
+  });
+});
